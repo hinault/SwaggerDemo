c29d8fa012dc9e05f6563a08bd4fe303c8b8ca20
Hinault <ngdonfack@gmail.com>
2017-11-19 17:21:46 -0500

    add comment

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -29,7 +29,13 @@ namespace SwaggerDemo.Controllers
             return Customers;
         }
 
-        // GET: api/Customers/5
+        /// <summary>
+        /// Retourne un client specifique à partir de son id
+        /// </summary>
+        /// <remarks>RAS</remarks>
+        /// <response code="200">client selectionné</response>
+        /// <response code="400">client introuvable pour l'id specifié</response>
+        /// <response code="500">Oops! le service est indisponible pour le moment</response>
         [HttpGet("{id}", Name = "GetById")]
         [ProducesResponseType(typeof(Customer), 200)]
         [ProducesResponseType(typeof(NotFoundResult), 404)]
